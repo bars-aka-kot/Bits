4db87042a6364caa3283252c7b4e681cc851253e
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-05-16 22:17:40 +0500

    add code

---
+++ IBits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bits
+{
+    internal interface IBits
+    {
+        public bool GetBit(int i);
+        public void SetBit(bool bit, int index);
+    }
+}
